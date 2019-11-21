10228b9126a7ff9042284afdcbf57746917d703b
dengsuc <vlannets@gmail.com>
2019-11-21 20:34:01 +0300

    Implement home work 1

---
+++ Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+	public static void main (String[] args) {
+		int cpu = 4; //Number of cores
+		byte sizeVcard = 64; //64 MB accelerator
+		short ram = 32; //Memory capacity
+		long sizeHdd = 1000000; //Size in MB
+		double frequency = 3.500d; //CPU frequency
+		float perfomance = 5.54f; //Perfomance pc
+		char symbol = 'X';
+		boolean check = true;
+		System.out.println(cpu);
+		System.out.println(sizeVcard);
+		System.out.println(ram);
+		System.out.println(sizeHdd);
+		System.out.println(frequency);
+		System.out.println(perfomance);
+		System.out.println(symbol);
+		System.out.println(check);
+	}
+}
