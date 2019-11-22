fd7e3cb7c28a2dc3b628b1fe1e918ef2e09a808e
Denis Martin <vlannets@gmail.com>
2019-11-22 19:33:07 +0300

    Modified today 22.11.2019 19.32 Cycle.java

--- lesson_1/Cycle.java
+++ lesson_1/Cycle.java
@@ -3,14 +3,16 @@ public class Cycle {
 		for (int i = 0; i <= 20; i++) {
 			System.out.println(i + " ");	
 		}
+
 		int k = 6;
 		while (k >= -6) {
 			System.out.println(k + " ");
-			k = k - 2;
+			k-=2;
 		}
+
 		int t = 9;
 		do {
-			t = t + 2;
+			t+=2;
 			System.out.println(t + " ");		
 		} while (t < 19);	
 	}
